d5e9844b9e8dd246482d5ce2ce0f899c787f4a14
AnandMuthukrishnan <anandaa.ramana2008@gmail.com>
2025-02-18 21:28:19 +0530

    Adding few more sample programs

---
+++ Selenium/src/main/java/week1/day2/PrimitiveDataType.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class PrimitiveDataType 
+{
+public static void main(String[] args) 
+{
+byte box = 1;
+byte box1 = 2;
+int box2;
+box2 = box * box1;
+System.out.println(box2);
+int a = 0;
+System.out.println(a);
+}
+}

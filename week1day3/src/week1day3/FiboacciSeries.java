8f4e3ce4dbdb0afa335cda4bd3609b4c23c9ba07
AnandMuthukrishnan <anandaa.ramana2008@gmail.com>
2025-02-20 22:48:37 +0530

    Create FiboacciSeries.java

---
+++ week1day3/src/week1day3/FiboacciSeries.java
@@ -0,0 +1,26 @@
+package week1day3;
+public class FiboacciSeries {
+
+	public static void main(String[] args) 
+	{
+		int a = 0,b=1;
+		int no_of_times=20;
+		for (int i = 0;i<no_of_times;i++)
+		{
+			if (i<2) 
+			{
+				System.out.println(i);
+			}
+		    else if(i%2==0)
+			{
+				a=a+b;
+				System.out.println(a);
+			}
+			else if(i%2 !=0)
+			{
+				b=a+b;
+				System.out.println(b);
+			}
+		}
+		}
+	}

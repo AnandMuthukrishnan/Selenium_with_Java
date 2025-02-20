4ed44575c5461c93c45eab1c27cc9c91c9260285
AnandMuthukrishnan <anandaa.ramana2008@gmail.com>
2025-02-20 22:55:09 +0530

    Update FiboacciSeries.java

--- week1day3/src/week1day3/FiboacciSeries.java
+++ week1day3/src/week1day3/FiboacciSeries.java
@@ -4,7 +4,7 @@ public class FiboacciSeries {
 	public static void main(String[] args) 
 	{
 		int a = 0,b=1;
-		int no_of_times=20;
+		int no_of_times=12;
 		for (int i = 0;i<no_of_times;i++)
 		{
 			if (i<2) 

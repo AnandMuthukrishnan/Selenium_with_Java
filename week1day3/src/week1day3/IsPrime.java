7efa274c262056e84c54a37093d33316725a262a
AnandMuthukrishnan <anandaa.ramana2008@gmail.com>
2025-02-20 13:16:06 +0530

    Update IsPrime.java

--- week1day3/src/week1day3/IsPrime.java
+++ week1day3/src/week1day3/IsPrime.java
@@ -5,17 +5,17 @@ public class IsPrime {
 	private static final boolean False = false;
 
 	public static void main(String[] args) {
-		int n = 49;
-		boolean isprime=true;
+		int n = 141;
+		boolean isprime = true;
 		for (int i = 2; i <= (n - 1); i++) {
 			if (n % i == 0) {
-				System.out.println(n+" can be divided by least number "+i);
-				isprime=false;
+				System.out.println(n + " can be divided by least number " + i);
+				isprime = false;
 				break;
-			} 
+			}
 		}
-		if (isprime==true) {
-			System.out.println("Prime NUmber");
+		if (isprime == true) {
+			System.out.println(n + " is a Prime NUmber");
 		}
 	}
 }

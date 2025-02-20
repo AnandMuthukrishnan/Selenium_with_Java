d738d7cb0a5131f013e81c6ce230c30cecf797e5
AnandMuthukrishnan <anandaa.ramana2008@gmail.com>
2025-02-20 13:21:36 +0530

    Create CheckNumberIsPositive.java

---
+++ week1day3/src/week1day3/CheckNumberIsPositive.java
@@ -0,0 +1,16 @@
+package week1day3;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int version = -10;
+		if (version >= 0) {
+			System.out.println(version + " is a positive number");
+		} else {
+			System.out.println(version + " is a negative number");
+		}
+
+	}
+
+}

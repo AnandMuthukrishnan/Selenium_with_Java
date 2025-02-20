53384bbd781e1acc945541c28ed12f3dfdd8f40e
AnandMuthukrishnan <anandaa.ramana2008@gmail.com>
2025-02-20 21:59:19 +0530

    Update IsPrime.java

--- week1day3/src/week1day3/IsPrime.java
+++ week1day3/src/week1day3/IsPrime.java
@@ -1,11 +1,9 @@
 package week1day3;
 
 public class IsPrime {
-	private static final boolean True = false;
-	private static final boolean False = false;
 
 	public static void main(String[] args) {
-		int n = 141;
+		int n = 49;
 		boolean isprime = true;
 		for (int i = 2; i <= (n - 1); i++) {
 			if (n % i == 0) {

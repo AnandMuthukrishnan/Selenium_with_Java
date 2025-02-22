5d8396f005eeb6f396c190df671a8c60af7e52e6
AnandMuthukrishnan <anandaa.ramana2008@gmail.com>
2025-02-22 22:08:15 +0530

    Update Finding_Missing_element.java

--- week1day4/src/week1day4/Finding_Missing_element.java
+++ week1day4/src/week1day4/Finding_Missing_element.java
@@ -1,13 +1,27 @@
 package week1day4;
+
 import java.util.Arrays;
-// to be completed
+
 public class Finding_Missing_element {
 	public static void main(String[] args) {
-		int a[] = {1, 4,3,2,8, 6, 7};
+		int a[] = { 1, 4, 3, 2, 8, 6, 7, 19,100 };
 		Arrays.sort(a);
-		for(int i=0;i<a.length;i++)
-		{
-			
+		for (int i = 0; i < a.length - 1; i++) {
+			if (a[i + 1] - a[i] > 1) 
+			{
+				if(a[i+1]-a[i] == 2)
+				{
+				System.out.println (a[i]+1);
+				}
+				else if(a[i+1]-a[i]>2)
+				{
+					for(int y =a[i];y<a[i+1];y++)
+					{
+						System.out.println(y);
+					}
+					
+				}
+			}
 		}
 	}
 }

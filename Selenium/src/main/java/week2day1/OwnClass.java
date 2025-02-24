e474a97b002498073310792fa699ba99520d540a
AnandMuthukrishnan <anandaa.ramana2008@gmail.com>
2025-02-24 12:08:03 +0530

    changes

--- Selenium/src/main/java/week2day1/OwnClass.java
+++ Selenium/src/main/java/week2day1/OwnClass.java
@@ -4,8 +4,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OwnClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		ChromeDriver driver=new ChromeDriver();
 		driver.get("http://leaftaps.com/opentaps/control/main");
+		Thread.sleep(5000);
+		driver.close();		
 		}
 }
